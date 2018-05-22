cf0907f76f0c4045b3dd0ffdac1a665f8cf250c6
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-05-22 16:31:06 +0530

    myservice.ts

--- src/app/services/my.service.ts
+++ src/app/services/my.service.ts
@@ -11,6 +11,19 @@ import { Observable} from 'rxjs/Observable';
 @Injectable()
 export class MyService {
 
-  constructor() { }
-
+  constructor(private _http:Http) { }
+  //obj from component
+  public myFun(obj):any{
+    return this._http.post("http://test-routes.herokuapp.com/test/uppercase",obj)
+    //map to catch the positive response from server
+    .map((response:Response)=>{
+      return response.json();
+    })
+    .catch(this._handleError);
+  }
+  public _handleError(err){
+    console.error("Error raised"+err);
+    //throw error with the help of Observable
+    return Observable.throw(err || "internal server error")
+  }
 }
