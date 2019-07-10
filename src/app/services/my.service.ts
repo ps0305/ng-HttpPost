7c175c8aa573ef178095dd9b4a52f742d4c3caef
Pankaj Singh <singh.pankaj@live.com>
2019-07-10 23:57:49 +0530

    Update my.service.ts

--- src/app/services/my.service.ts
+++ src/app/services/my.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 //import 'Http' to hit the server
 //import 'Response' as response from server is 'Response' type
-import { Http, Response} from '@angular/http';
+import { 
+  Http,
+  Response
+} from '@angular/http';
 //import rxjs/Rx as map(),catch() and throw function available in the package
 import "rxjs/Rx";
 //import 'Observable' to throw the error
-import { Observable} from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 
-
+//Decorator that marks a class as available to Injector for creation.
 @Injectable()
 export class MyService {
 
@@ -15,11 +18,11 @@ export class MyService {
   //obj from component
   public myFun(obj):any{
     return this._http.post("http://test-routes.herokuapp.com/test/uppercase",obj)
-    //map to catch the positive response from server
+    //map to catch the positive observable response from server
     .map((res:Response)=>{
       return res.json();
     })
-    .catch(this._handleError);
+    .catch(this._handleError); //catch to catch the negative/error observable response from server
   }
   public _handleError(err){
     console.error("Error raised"+err);
